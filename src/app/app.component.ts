7901765a03960bba906e1543d59f40ed34eef134
ShubhamDhivar <dhivarshubham7@gmail.com>
2022-12-07 12:50:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TypingMaster';
+  randomText = faker.hacker.phrase();
+  userInput = "";
+  
+  onInput(vada: Event) {
+    this.userInput = (vada.target as HTMLInputElement).value
+    console.log(this.userInput)
+  }
+
+  compare(randomLetter: string, userInput: string){
+    if(!userInput) return 'pending';
+
+    return randomLetter === userInput ? 'correct' : 'incorrect';
+  }
+  
+  
 }
